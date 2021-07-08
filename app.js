a29c5e50f83b362aaac8c91f8e0326fbc380b860
Karl Keller <kgk86@hotmail.com>
2021-07-08 20:52:08 +0200

    Hello world accepting a POST or params for whom to say hello to

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+  console.table(req.query)
+  let greeting = req.query.message || "World"
+  res.send(`<h1>Hello ${greeting}!</h1>`)
+})
+
+app.post("/", (req, res) => {
+  console.table(req.body)
+  res.send({message: req.body})
+})
+
+app.listen(port, () => console.warn(`Server is running on
+http://localhost:${port}!`))
