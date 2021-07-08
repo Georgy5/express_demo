418f89083255b780fc4e93f120275130b5ce4386
Karl Keller <kgk86@hotmail.com>
2021-07-08 22:21:14 +0200

    gitignore node_modules folder

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,12 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
-const app = require('../app');
+const app = require("../app");
 const supertest = require('supertest');
 
 let server, request, response;
 
 before((done) => {
+  console.log("Hello app");
   server = app.listen(done);
+  console.log(server);
   request = supertest.agent(server);
 });
 
@@ -16,13 +18,15 @@ after((done) => {
 
 describe("GET /", () => {
   beforeEach(async () => {
-    response = await request.get("/?message=Uranus")
+    console.log(request);
+    response = await request.get("/?message=Uranus");
   });
 
   it("is expected to respond with status 200", () => {
+    console.log("Hello");
     expect(response.status).to.equal(200);
   });
-
+  
   it("is expected to respond with some HTML", () => {
     expect(response.text).to.equal('<h1>Hello Uranus!</h1>');
   });
